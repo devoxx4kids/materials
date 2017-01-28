2596d7ba93b7c80db34e231ba026ad0ec711662e
laorine <laostreet934@gmail.com>
2017-01-28 09:50:22 -0500

    New project

---
+++ workshops/android/workshop_01_ObitalD4K/OrbitaD4K/app/src/main/java/org/devoxx4kids/orbitad4k/MainActivity.java
@@ -0,0 +1,13 @@
+package org.devoxx4kids.orbitad4k;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
