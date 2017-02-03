2d76a54597c0e666792807a1b3809c01eed0d5f4
laorine <laostreet934@gmail.com>
2017-02-03 18:59:07 -0500

    adding java interaction

--- workshops/android/workshop_01_ObitalD4K/OrbitaD4K/app/src/main/java/org/devoxx4kids/orbitad4k/MainActivity.java
+++ workshops/android/workshop_01_ObitalD4K/OrbitaD4K/app/src/main/java/org/devoxx4kids/orbitad4k/MainActivity.java
@@ -1,13 +1,37 @@
 package org.devoxx4kids.orbitad4k;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final double RADIUS = 6_378.14;
+    private static final double GRAVITY = 6.67 * Math.pow(10, -11);
+    private static final double EARTH_MASS = 5.9736 * Math.pow(10, 24);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculate(View view) {
+        EditText editView = (EditText)findViewById(R.id.editText);
+        Integer distance = Integer.valueOf(editView.getText().toString());
+
+        double period = Math.sqrt(4 * Math.pow(Math.PI, 2) * Math.pow((distance + RADIUS) * 1000, 3) / (GRAVITY * EARTH_MASS));
+
+        TextView resultView = (TextView) findViewById(R.id.textView);
+        String hms = String.format("%02dh %02dm %02ds", TimeUnit.SECONDS.toHours((long) period),
+                TimeUnit.SECONDS.toMinutes((long) period) % 60, (long) period % 60);
+        resultView.setText(hms);
     }
 }
