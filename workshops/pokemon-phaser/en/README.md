b152fe0246627b2c22e959a014748a09bc4a2fbf
Wojtek Wilk <wilk.wojtek@gmail.com>
2017-04-29 00:40:25 +0200

    initial commit - pokemon phaser

---
+++ workshops/pokemon-phaser/en/README.md
@@ -0,0 +1,15 @@
+Pokemon with Phaser
+====
+Workshop was presented for the first time during Devoxx4Kids at Devoxx US in March 2017 in San Jose.
+It's a simple game written in Javascript and HTML. It uses a game engine called Phaser. Participants are not expected to write this game from scratch, only to make some modifications to it.
+
+Workshop is split into a few parts:
+
+* quick presentation of a game itself
+* a few exercises to practice simple JavaScript structures (variables, functions, conditional statements)
+* explanation how code is connected to what participants see in the game
+* time for modifications
+
+Examplary modifications are mentioned in the presentation.
+
+
---
+++ workshops/pokemon-phaser/en/app/main.js
@@ -0,0 +1,67 @@
+// Initialize Phaser, and create a 800x480px game
+var screenWidth = 800;
+var screenHeight = 480;
+var game = new Phaser.Game(screenWidth, screenHeight, Phaser.CANVAS, 'gameContainer');
+
+var updatesCounter = 0;
+var caughtPokemons = 0;
+var text = null;
+
+// Create our 'main' state that will contain the game
+var mainState = {
+
+    preload: function() {
+    	game.load.crossOrigin = 'anonymous';
+      	
+      	// Load all needed images
+      	game.load.image('background', 'http://d4k.wwilk.com/assets/pokemon_background.png');
+      	game.load.image('pokemon', 'http://d4k.wwilk.com/assets/pikachu.png');
+    },
+
+    create: function() {
+      	// Set the background image
+    	game.add.tileSprite(0, 0, screenWidth, screenHeight, 'background');  
+    	// Set the physics system
+     	game.physics.startSystem(Phaser.Physics.ARCADE);
+     
+     	// Create text container and set it to initial value
+     	text = game.add.text(0, 0, 'Catch a Pokemon by clicking on it', { fill: '#ffffff'});
+    },
+     
+    update: function() {
+		updatesCounter++;
+		// Create pokemon only every 100th call of update function
+        if(updatesCounter % 100 == 0){
+	     	createPokemon();    
+        }
+    }
+
+};
+
+function createPokemon() {
+	// Select initial x position of the pokemon randomly
+ 	var horizontalPosition = parseInt(screenWidth * Math.random());
+ 	var verticalPosition = 0;
+
+    // Display the Pokemon on the screen
+    var pokemon = game.add.sprite(horizontalPosition, verticalPosition, 'pokemon');
+
+	// Enables all kind of input actions on this image (click, etc)
+    pokemon.inputEnabled = true;
+		
+	pokemon.events.onInputDown.add(function(){
+      	caughtPokemons++;
+      	text.text = 'Congratulations, you caught ' + caughtPokemons + ' Pokemon';
+      	pokemon.destroy();
+    }, this);
+
+    // Add gravity to the pokemon to make it fall
+    game.physics.arcade.enable(pokemon);
+    pokemon.body.gravity.y = 300;
+};
+
+
+// Add and start the 'main' state to start the game
+game.state.add('main', mainState);
+game.state.start('main');
+
